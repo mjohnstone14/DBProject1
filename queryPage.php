a5547e409a9dbc9032b2a587a90dc8c0e36d4203
mjohnstone14 <mjohnstone@eaglerockschool.org>
2018-10-18 18:57:02 -0700

    coded up the queryPage.php and moved it to var/www/html in server

--- queryPage.php
+++ queryPage.php
@@ -1,7 +1,31 @@
 <!DOCTYPE html>
 <html>
-<body>
+    <head>
+        <link rel ="stylesheet" type = "text/css" href="core.css">
+        <h1>Query Form</h1>
+    </head>
+    <body>
+    <div class="content">
+        <form action="showPassengers.php" method="get">
+            SSN: <input name="passenger_ssn" type="text" size="25" />
 
-<?php
-</body>
+            <input name="mySubmit" type="submit" value="submit" />
+        </form>
+    </div>
+    <?php
+    try {
+        //path to the SQLite database file
+        $db_file = './myDB/airport.db';
+
+        $db = new PDO('sqlite:' . $db_file);
+        //set errormode to use exceptions
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $db->prepare("SELECT * FROM PASSENGERS where ssn = ?");
+      
+    } catch(PDOException $e) {
+        die('Exception : '.$e->getMessage());
+    }
+    ?>
+    </body>
 </html>
