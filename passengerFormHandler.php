dedd9fcb80e13287e848f32133459a659039b538
mjohnstone14 <mjohnstone@eaglerockschool.org>
2018-10-18 19:57:08 -0700

    updated passengerFormHandler and began some checks, needs work

--- passengerFormHandler.php
+++ passengerFormHandler.php
@@ -4,6 +4,17 @@ $m_name = $_POST["m_name"];
 $last_name = $_POST["last_name"];
 $ssn = $_POST["SSN"];
 
+if(empty($first_name)) {
+    header('Location: passengerForm.html');
+    exit;
+} else if(empty($last_name)) {
+    header('Location: passengerForm.html');
+    exit;
+} else if(empty($ssn)) {
+    header('Location: passengerForm.html');
+    exit;
+} else {
+    echo $first_name;
+}
 
-echo $first_name;
 ?>
