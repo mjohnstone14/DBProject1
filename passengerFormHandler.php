ba8cad2d0cb50fba92f643584d297ba2df677400
mjohnstone14 <mjohnstone@eaglerockschool.org>
2018-10-18 19:26:03 -0700

    added code to passengerFormHandler

--- passengerFormHandler.php
+++ passengerFormHandler.php
@@ -1,7 +1,9 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-</body>
-</html>
+$first_name = $_POST["first_name"];
+$m_name = $_POST["m_name"];
+$last_name = $_POST["last_name"];
+$ssn = $_POST["SSN"];
+
+
+echo $first_name;
+?>
