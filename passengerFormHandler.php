140bf0d54e46e213e9e76605483e92e24543415e
mjohnstone14 <mjohnstone@eaglerockschool.org>
2018-10-19 11:01:38 -0700

    modified passengerForm and handler to handle checks and specify requirements

--- passengerFormHandler.php
+++ passengerFormHandler.php
@@ -4,10 +4,15 @@ $m_name = $_POST["m_name"];
 $last_name = $_POST["last_name"];
 $ssn = $_POST["SSN"];
 
-if(empty($first_name)) {
+if(strlen($ssn) > 11) {
     header('Location: passengerForm.html');
     exit;
-} else if(empty($last_name)) {
+}
+
+if(empty($first_name) || !is_string($first_name) || preg_match('#[0-9]#', $first_name)) {
+    header('Location: passengerForm.html');
+    exit;
+} else if(empty($last_name) || !is_string($first_name) || preg_match('#[0-9]#', $last_name)) {
     header('Location: passengerForm.html');
     exit;
 } else if(empty($ssn)) {
