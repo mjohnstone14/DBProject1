72b792305975baeee088110f883a32f16e65ece1
unknown <harper.shapiro@gmail.com>
2018-10-20 11:21:31 -0700

    quote typos

--- queryPage.php
+++ queryPage.php
@@ -20,12 +20,13 @@
            $db = new PDO('sqlite:' . $db_file);
            //set errormode to use exceptions
            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           $stmt = $db->prepare("SELECT * from  passengers  where :statement");
+           $stmt = $db->prepare('SELECT * from  passengers  where :statement');
            //bind to post values
            $stmt->bindParam(':statement',$_POST["statement"]);
            $stmt->execute();
            //disconnect
-            $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC);
+           $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC);
+           var_dump($result_set);
            foreach($result_set as $tuple) {
                     echo "<font color='blue'>$tuple[ssn]</font> $tuple[f_name] $tuple[m_name] $tuple[l_name]";
            }
