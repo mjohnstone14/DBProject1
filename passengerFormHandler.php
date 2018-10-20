5ef4f703fb35776898c2939cc134d304007edf80
mjohnstone14 <mjohnstone@eaglerockschool.org>
2018-10-20 08:32:08 -0700

    passengerForm

--- passengerFormHandler.php
+++ passengerFormHandler.php
@@ -4,19 +4,34 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $m_name = $_POST["m_name"];
     $last_name = $_POST["last_name"];
     $ssn = $_POST["ssn"];
+    $error = NULL;
+    $message = NULL;
 
-    if(strlen($ssn) != 11) {
-        header('Location: passengerForm.html');
-        exit;
-    }
     if(empty($first_name) || !is_string($first_name) || preg_match('#[0-9]#', $first_name)) {
-        header('Location: passengerForm.html');
-        exit;
-    } else if(empty($last_name) || !is_string($first_name) || preg_match('#[0-9]#', $last_name)) {
-        header('Location: passengerForm.html');
-        exit;
+        $error = "error=true";
+    } else if(empty($last_name) || !is_string($last_name) || preg_match('#[0-9]#', $last_name)) {
+        if(isset($error)) {
+            $error .= "&error2=true";
+        } else {
+            $error = "error2=true";
+        }
     } else if(empty($ssn)) {
-        header('Location: passengerForm.html');
+        if(isset($error)) {
+            $error .= "&error3=true";
+        } else {
+            $error = "error3=true";
+        }
+    }
+    else if(strlen($ssn) != 11) {
+        if(isset($error)) {
+            $error .= "&error4=true";
+        } else {
+            $error = "error4=true";
+        }
+    } 
+
+    if(isset($error)) {
+        header('Location: passengerForm.php?'.$error);
         exit;
     } else {
         
@@ -29,7 +44,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         //bind to post values
         $stmt->bindParam(':f_name', $first_name);
         $stmt->bindParam(':m_name', $m_name);
-        $stmt->bindParam(':l_name', $l_name);
+        $stmt->bindParam(':l_name', $last_name);
         $stmt->bindParam(':ssn',$ssn); 
         $stmt->execute();
         //disconnect
@@ -37,8 +52,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         } catch(PDOException $e) {
          die('Exception : '.$e->getMessage());
          }
-
-        header("Location: showPassengers.php");
+        $message = "success";
+        header("Location: showPassengers.php?".$message);
         
     }
 }
