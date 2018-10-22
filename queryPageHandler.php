8e9ae59844f2f6050a2e287357ed3b1b860bf870
unknown <harper.shapiro@gmail.com>
2018-10-22 14:32:09 -0700

    fix on error checking in queryPageHandler

--- queryPageHandler.php
+++ queryPageHandler.php
@@ -1,30 +1,12 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $statement=$_POST["statement"];
     $error = NULL;
     $message = NULL;
 
-    if(empty($first_name) || !is_string($first_name) || preg_match('#[0-9]#', $first_name)) {
+    if(empty($statement)) {
         $error = "error=true";
-    } else if(empty($last_name) || !is_string($last_name) || preg_match('#[0-9]#', $last_name)) {
-        if(isset($error)) {
-            $error .= "&error2=true";
-        } else {
-            $error = "error2=true";
-        }
-    } else if(empty($ssn)) {
-        if(isset($error)) {
-            $error .= "&error3=true";
-        } else {
-            $error = "error3=true";
-        }
-    }
-    else if(strlen($ssn) != 11) {
-        if(isset($error)) {
-            $error .= "&error4=true";
-        } else {
-            $error = "error4=true";
-        }
-    }
+      }
 
     if(isset($error)) {
         header("Location: queryPage.php?".$error);
@@ -43,7 +25,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                $stmt = $db->prepare("SELECT * from  passengers WHERE :statement;");
                //bind to post values
-               $stmt->bindParam(':statement',$_POST["statement"]);
+               $stmt->bindParam(':statement',$statement);
                $stmt->execute();
                //disconnect
                $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC);
