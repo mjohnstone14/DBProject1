a219eb60320544165bd45b055549e890fee5b34a
unknown <harper.shapiro@gmail.com>
2018-10-22 14:39:04 -0700

    small fix

--- queryPageHandler.php
+++ queryPageHandler.php
@@ -38,7 +38,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
            }
         }
         $message = "success";
-        header("Location: queryPage.php?results=$result_set&statement=$statement".$message);
+        header("Location: queryPage.php?results=$result_set&statement=$statement");
 
     }
 }
