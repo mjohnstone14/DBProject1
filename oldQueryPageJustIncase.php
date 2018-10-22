c11d38d3d5d6dc8512fb4f46d21b39990f466c44
Ben Shapiro <bshapiro@pugetsound.edu>
2018-10-21 23:18:37 -0700

    adding old query page as backup

---
+++ oldQueryPageJustIncase.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel ="stylesheet" type = "text/css" href="core.css">
+        <h1>Query Form</h1>
+    </head>
+    <body>
+    <div class="content">
+        <form action="queryPage.php" method="POST">
+            select from passengers where  <input name="statement" type="text" size="75" />
+          <input name="submit" type="submit">
+        </form>
+    </div>
+    <?php
+    if(isset($_POST["statement"])){
+       try {
+           //path to the SQLite database file
+           $db_file = './myDB/airport.db';
+
+           $db = new PDO('sqlite:' . $db_file);
+           //set errormode to use exceptions
+           $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           $stmt = $db->prepare('SELECT * from  passengers WHERE :statement;');
+           //bind to post values
+           $stmt->bindParam(":statement",$_POST["statement"]);
+           $stmt->execute();
+           //disconnect
+           $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC);
+           var_dump($result_set);
+           foreach($result_set as $tuple) {
+                    echo "<font color='blue'>$tuple[ssn]</font> $tuple[f_name] $tuple[m_name] $tuple[l_name]";
+           }
+           $db = null;
+       } catch(PDOException $e) {
+           die('Exception : '.$e->getMessage());
+       }
+    }
+    ?>
+    </body>
+</html>
