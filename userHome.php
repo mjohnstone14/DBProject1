5521a5870b913f0f74066ba535676b9a15142bd9
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-13 16:02:36 -0800

    userHome update

--- userHome.php
+++ userHome.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <link rel ="stylesheet" type = "text/css" href="test.css">
+  </head>
 <!-- home page once logged in. includes current built deck
 and usernames of recent posters of cards. Also has a nav
 bar -->
