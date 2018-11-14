2c6dbde5fc98f2673b8e7ab7b28b16b844479d48
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-14 15:35:49 -0800

    user home update and about us

---
+++ aboutUs.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel ="stylesheet" type = "text/css" href="core.css">
+    <h1>About Us, Greenhouse Goons</h1>
+  </head>
+<!-- home page once logged in. includes current built deck
+and usernames of recent posters of cards. Also has a nav
+bar -->
+</html>
