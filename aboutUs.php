48a815ff4c235fcec0a87e00f247206073e91f8d
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-15 10:44:22 -0800

    about us update

--- aboutUs.php
+++ aboutUs.php
@@ -1,10 +1,187 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <link rel ="stylesheet" type = "text/css" href="core.css">
-    <h1>About Us, Greenhouse Goons</h1>
-  </head>
-<!-- home page once logged in. includes current built deck
-and usernames of recent posters of cards. Also has a nav
-bar -->
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.8.7, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.8.7, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+
+
+</head>
+<body>
+  <section class="features16 cid-r9uPH065pS" id="features16-3">
+
+
+
+    <div class="container align-center">
+        <h2 class="pb-3 mbr-fonts-style mbr-section-title display-2">
+            OUR AWESOME TEAM
+        </h2>
+        <h3 class="pb-5 mbr-section-subtitle mbr-fonts-style mbr-light display-5">
+            Click any icon or text to edit or style it. Click blue "Gear" icon in the top right corner to hide/show text and change the block and cards background, icons count and other.
+        </h3>
+        <div class="row media-row">
+
+        <div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="assets/images/face6.jpg">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           John Smith
+                        </p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/mobirise" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.instagram.com/mobirise/" target="_blank">
+
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">
+
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="assets/images/face6.jpg">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           John Smith
+                        </p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/mobirise" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.instagram.com/mobirise/" target="_blank">
+
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">
+
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="assets/images/face6.jpg">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           John Smith
+                        </p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/mobirise" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.instagram.com/mobirise/" target="_blank">
+
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">
+
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="assets/images/face6.jpg">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           John Smith
+                        </p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/mobirise" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.instagram.com/mobirise/" target="_blank">
+
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">
+
+                        </a>
+                    </div>
+                </div>
+            </div></div>
+    </div>
+</section>
+
+
+  <section class="engine"><a href="https://mobirise.info/o">portfolio site templates</a></section><script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+
+
+</body>
 </html>
