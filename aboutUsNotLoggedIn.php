11cc19a029568745dc29600d56feebf883149267
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-16 08:56:04 -0500

    home page

---
+++ aboutUsNotLoggedIn.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <form method="get" action="homePage.php">
+    <button type="submit">Home Page</button>
+  </form>
+  <!-- Site made with Mobirise Website Builder v4.8.7, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.8.7, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+
+
+</head>
+<body>
+  <section class="features16 cid-r9uPH065pS" id="features16-3">
+
+
+
+    <div class="container align-center">
+        <h2 class="pb-3 mbr-fonts-style mbr-section-title display-2">
+            OUR AWESOME TEAM
+        </h2>
+
+        <div class="row media-row">
+
+        <div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="lib/images/harperShapiro.jpg">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           Harper Shapiro
+                        </p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/mobirise" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.instagram.com/mobirise/" target="_blank">
+
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">
+
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="lib/images/benShapiro.jpg">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           Ben Shapiro
+                        </p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/mobirise" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.instagram.com/mobirise/" target="_blank">
+
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">
+
+                        </a>
+                    </div>
+                </div>
+            </div><div class="team-item col-lg-3 col-md-6">
+                <div class="item-image">
+                    <img src="lib/images/marwan.jpg">
+                </div>
+                <div class="item-caption py-3">
+                    <div class="item-name px-2">
+                        <p class="mbr-fonts-style display-5">
+                           Marwan
+                        </p>
+                    </div>
+                    <div class="item-role px-2">
+                        <p>Developer</p>
+                    </div>
+                    <div class="item-social pt-2">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="p-1 socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="p-1 socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                            <span class="p-1 socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/mobirise" target="_blank">
+                            <span class="p-1 socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                        <a href="https://www.instagram.com/mobirise/" target="_blank">
+
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCt_tncVAetpK5JeM8L-8jyw" target="_blank">
+
+                        </a>
+                    </div>
+                </div>
+            </div>
+            </div></div>
+    </div>
+</section>
+
+
+  <section class="engine"><a href="https://mobirise.info/o">portfolio site templates</a></section><script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+
+
+</body>
+</html>
--- homePage.php
+++ homePage.php
@@ -60,7 +60,7 @@ body, html {
       <i class="fa fa-bars"></i>
     </a>
     <a href="#home" class="w3-bar-item w3-button">HOME</a>
-    <a href="aboutUs.php" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
+    <a href="aboutUsNotLoggedIn.php" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
     <a href="signup_form.php" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i> Sign Up</a>
     <a href="login_form.php" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i> Log In</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
@@ -70,7 +70,7 @@ body, html {
 
   <!-- Navbar on small screens -->
   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-    <a href="aboutUs.php" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
+    <a href="aboutUsNotLoggedIn.php" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
     <a href="signup_form.php" class="w3-bar-item w3-button" onclick="toggleFunction()">Sign Up</a>
     <a href="login_form.php" class="w3-bar-item w3-button" onclick="toggleFunction()">Log In</a>
     <a href="#" class="w3-bar-item w3-button">SEARCH</a>
