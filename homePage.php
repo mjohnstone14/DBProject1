7b969dd1e823a283e4c09c19a92643abdaa21c84
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-16 08:32:27 -0500

    home page

--- homePage.php
+++ homePage.php
@@ -60,9 +60,9 @@ body, html {
       <i class="fa fa-bars"></i>
     </a>
     <a href="#home" class="w3-bar-item w3-button">HOME</a>
-    <a href="#about" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
-    <a href="#portfolio" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i> PORTFOLIO</a>
-    <a href="#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i> CONTACT</a>
+    <a href="aboutUs.php" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
+    <a href="signup_form.php" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i> Sign Up</a>
+    <a href="login_form.php" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i> Log In</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
       <i class="fa fa-search"></i>
     </a>
@@ -70,9 +70,9 @@ body, html {
 
   <!-- Navbar on small screens -->
   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-    <a href="#about" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
-    <a href="#portfolio" class="w3-bar-item w3-button" onclick="toggleFunction()">PORTFOLIO</a>
-    <a href="#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
+    <a href="aboutUs.php" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
+    <a href="signup_form.php" class="w3-bar-item w3-button" onclick="toggleFunction()">Sign Up</a>
+    <a href="login_form.php" class="w3-bar-item w3-button" onclick="toggleFunction()">Log In</a>
     <a href="#" class="w3-bar-item w3-button">SEARCH</a>
   </div>
 </div>
