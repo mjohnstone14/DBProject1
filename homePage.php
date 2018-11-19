d9abc379cc5a9664b73259c59202d132f408063f
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-19 15:13:00 -0500

    home page

--- homePage.php
+++ homePage.php
@@ -140,7 +140,7 @@ body, html {
 <!-- Second Parallax Image with Portfolio Text -->
 <div class="bgimg-2 w3-display-container w3-opacity-min">
   <div class="w3-display-cell-top">
-    <span class="w3-xxlarge w3-text-dark-grey">PORTFOLIO</span>
+    <span class="w3-xxlarge w3-text-dark-grey w3-large">PORTFOLIO</span>
   </div>
 </div>
 
