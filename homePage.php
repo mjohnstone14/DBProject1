105b378de701dc6e72260a8e67245b1f81ac756f
Ben Shapiro <bshapiro@pugetsound.edu>
2018-11-18 21:36:00 -0500

    home page

--- homePage.php
+++ homePage.php
@@ -25,19 +25,19 @@ body, html {
 /* First image (Logo. Full height) */
 .bgimg-1 {
     background-image: url("http://www.besthdwallpaper.co/wp-content/uploads/2018/05/hd-3d-4k-wallpaper-57-images.jpg");
-    min-height: 100%;
+    min-height: 50%;
 }
 
 /* Second image (Portfolio) */
 .bgimg-2 {
     background-image: url("https://img.heypik.com/58pic/19/03/59/07z58PICumw_1024.jpg?x-oss-process=image/resize,w_264/sharpen,100/quality,q_80");
-    min-height: 400px;
+    min-height: 200px;
 }
 
 /* Third image (Contact) */
 .bgimg-3 {
     background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1Ee8HFRc-Q-cY61CchtGqVXLINjedi4JUqewkplT-wnigLh8Ag");
-    min-height: 400px;
+    min-height: 200px;
 }
 
 .w3-wide {letter-spacing: 10px;}
