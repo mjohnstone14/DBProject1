26f3623f8220586681746d1b673f3385d9ca5855
unknown <harper.shapiro@gmail.com>
2018-11-28 17:16:09 -0800

    typo-----------

--- tradeForm.php
+++ tradeForm.php
@@ -1,26 +1,28 @@
-
+<!DOCTYPE html>
 <html>
 <head> <?php session_start(); ?>  </head>
+  <h1> Initiate Trade </h1>
   <body>
-  <p> hello </p>
    <?php
-  $trade_user = $_POST['trade_user'];
-  $initiator = $_SESSION['username'];
+
+     $trade_user = $_POST['trade_user'];
+     $initiator = $_SESSION['username'];
 
 
      $db_file = '../myDB/spitting.db';
      $db = new PDO('sqlite:' . $db_file);
 
-      set errormode to use exceptions
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      //set errormode to use exceptions
+     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-      $stmt = $db->prepare('SELECT amount,imagePath FROM User NATURAL JOIN Owns NATURAL JOIN Card where username=:username');
-      $stmt->bindParam(':username',$initiator);
-      $result = $stmt->execute();
-      $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC);
+     $stmt = $db->prepare('SELECT amount,imagePath FROM User NATURAL JOIN Owns NATURAL JOIN Card where username=:username');
+     $stmt->bindParam(':username',$initiator);
+     $result = $stmt->execute();
+     $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC);
       foreach($result_set as $path) {
       echo "<img src = $path[imagePath] height=20% width=20% >";
+    }
      ?>
   </body>
 </html>
