e8b6548b56e974b6809ac15b80d00cd23ae80d09
unknown <harper.shapiro@gmail.com>
2018-11-28 17:11:15 -0800

    work on trade form

--- tradeForm.php
+++ tradeForm.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head> <?php session_start(); ?> </head>
 
-<body>
-   
+<html>
+<head> <?php session_start(); ?>  </head>
+  <body>
+  <p> hello </p>
    <?php
-     $trade_user = $_POST['trade_user'];
-     $initiator = $_SESSION['username'];
+  $trade_user = $_POST['trade_user'];
+  $initiator = $_SESSION['username'];
+
 
      $db_file = '../myDB/spitting.db';
      $db = new PDO('sqlite:' . $db_file);
 
-      //set errormode to use exceptions
+      set errormode to use exceptions
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
@@ -20,12 +20,7 @@
       $result = $stmt->execute();
       $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC);
       foreach($result_set as $path) {
-       echo "<img src = $path[imagePath] height=20% width=20% >";
+      echo "<img src = $path[imagePath] height=20% width=20% >";
      ?>
-
-</body>
+  </body>
 </html>
-
-
-
-
