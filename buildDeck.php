ccc1aa3e8200ad840e1de0e6876f180a2dcf59dc
unknown <harper.shapiro@gmail.com>
2018-12-02 13:01:40 -0800

    work on buildDeck

--- buildDeck.php
+++ buildDeck.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
+<body>
 <h1> Build Your Deck </h1>
 <p> Select up to 10 cards to add to your deck. </p>
-
+<form action='buildDeckHandler.php'>
 <?php
   session_start();
   $creator = $_SESSION['username'];
@@ -16,11 +17,13 @@
   $result = $stmt->execute();
   $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC);
   foreach($result_set as $path) {
-    echo "<form action='buildDeckHandler.php'>
-    <input type='checkbox' name=$path[cardID]>
+    echo "<input type='checkbox' name=$path[cardID]>
     <img src = $path[imagePath] height=30% width=30% >
-    </form>";
+    </input>
+    ";
   }
-  
 ?>
+  <input type="submit"\>
+  </form>
+</body>
 </html>
