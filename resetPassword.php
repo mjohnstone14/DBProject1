29d63f4e5ef9efb13368154e088c03347866844a
mjohnstone14 <mjohnstone@eaglerockschool.org>
2018-12-02 15:00:34 -0800

    fiddling around

--- resetPassword.php
+++ resetPassword.php
@@ -1,5 +1,20 @@
 <?php
 echo '<link rel ="stylesheet" type = "text/css" href="templateCSS.css">';
+	function random_str(
+		$length,
+		$keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	) {
+		$str = '';
+		$max = mb_strlen($keyspace, '8bit') - 1;
+		if ($max < 1) {
+			throw new Exception('$keyspace must be at least two characters long');
+		}
+		for ($i = 0; $i < $length; ++$i) {
+			$str .= $keyspace[random_int(0, $max)];
+		}
+		return $str;
+	}
+
 	if(isset($_POST['login'])) {
 		$db = new PDO('sqlite:../myDB/spitting.db');
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -43,20 +58,7 @@ echo '<link rel ="stylesheet" type = "text/css" href="templateCSS.css">';
 		}
 	}
 
-	function random_str(
-		$length,
-		$keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	) {
-		$str = '';
-		$max = mb_strlen($keyspace, '8bit') - 1;
-		if ($max < 1) {
-			throw new Exception('$keyspace must be at least two characters long');
-		}
-		for ($i = 0; $i < $length; ++$i) {
-			$str .= $keyspace[random_int(0, $max)];
-		}
-		return $str;
-	}
+	
 ?>
 
 <html>
