f87eeba9b3a5f00d010258daab8b24a559bf4b55
Ben Shapiro <bshapiro@pugetsound.edu>
2018-12-03 03:49:16 -0800

    updated search query

--- searchQuery.php
+++ searchQuery.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Search Results</title>
+  <form method="get" action="userHome.php">
+    <button type="submit">Home Page</button>
+  </form>
+</head>
+<title>Spitting Image</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="w3-main" style="margin-left:250px">
+
+  <!-- Push down content on small screens -->
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+
+  <!-- Top header -->
+  <header class="w3-container w3-xlarge">
+    <p class="w3-left">All Users Cards</p>
+  </header>
+  <div class="w3-container w3-black w3-padding-32">
+  <div class="w3-container w3-text-grey" id="Other Users">
+    <p>All items</p>
+  </div>
+
 <?php
 session_start();
 $username = $_POST['username'];
@@ -15,7 +44,15 @@ $username = $_POST['username'];
   foreach($result_set as $path) {
     $imagePath = $path['imagePath'];
     $cardName = $path['cardName'];
-    echo "<img src = $path[imagePath] height=100% width=100%</img>";
+    echo "<div class = 'w3-row-w3'>";
+    echo "<div class = 'w3-col l3 s6'>";
+    echo "<div class = 'w3-container'>";
+    echo "<form method='get' action='./tradeForm.php'>";
+    echo "<img src = $path[imagePath] height=100% width=100%</img>";    echo "</form>";
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+
   }
   ?>
 
