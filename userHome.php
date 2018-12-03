2a46329a6d2f5b91a84763c6e5b7a27bbd30e13d
unknown <harper.shapiro@gmail.com>
2018-12-02 18:18:21 -0800

    text color:
    '

--- userHome.php
+++ userHome.php
@@ -95,7 +95,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     echo "<div class = 'w3-col l3 s6'>";
     echo "<div class = 'w3-container'>";
     echo "<form method='get' action='./tradeForm.php'>";
-    echo "<p> $path[username] </p>";
+    echo "<p style="color: white"> $path[username] </p>";
     echo "<img src = $path[imagePath] height=90% width=90%><button class='w3-button w3-black'>Trade Now</button></img>";
     echo "</form>";
     echo "</div>";
