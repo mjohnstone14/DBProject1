3130f0a15279624cf24628679997ac34a9537611
Ben Shapiro <bshapiro@pugetsound.edu>
2018-12-03 02:26:28 -0800

    updated user home

--- userHome.php
+++ userHome.php
@@ -65,7 +65,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
   <header class="w3-container w3-xlarge">
     <p class="w3-left">Other Users</p>
     <p class="w3-right">
-      <i href="buildDeck.php" class="fa fa-clone w3-margin-right"</i>
+      <i class="fa fa-clone w3-margin-right" href="buildDeck.php"</i>
       <i class="fa fa-search"></i>
     </p>
   </header>
