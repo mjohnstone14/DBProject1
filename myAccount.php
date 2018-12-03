8c21e86ce1a353607f00b6121d94f4fdc39a4593
mjohnstone14 <mjohnstone@eaglerockschool.org>
2018-12-03 15:00:06 -0800

    bless ur soul

--- myAccount.php
+++ myAccount.php
@@ -6,10 +6,10 @@ echo '<link rel ="stylesheet" type = "text/css" href="templateCSS.css">';
 
 		$errMsg = '';
 		// Get data from FORM
-        $email = $_POST['username'];
+        $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $info = [':username' => username, ':password' => $password];
+        $info = [':username' => $username, ':password' => $password];
         $sql = 'UPDATE user SET password=:password WHERE username=:username';
         $query= $db->prepare($sql);
         $query->execute($info);
