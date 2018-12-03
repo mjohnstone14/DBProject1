f4f60cc9cdcf725b7e95b42414d3a000d5696076
Ben Shapiro <bshapiro@pugetsound.edu>
2018-12-02 17:25:40 -0800

    user home search

--- userHome.php
+++ userHome.php
@@ -110,7 +110,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
   <div class="w3-container w3-black w3-padding-32">
     <h1>Search</h1>
     <p>To find users by username!</p>
-    <p><input class="w3-input w3-border" type="text" placeholder="Enter username" style="width:100%"></p>
+    <p><input class="w3-input w3-border" type="text" placeholder="Enter username" name = 'username' style="width:100%"></p>
     <button type="submit" class="w3-button w3-red w3-margin-bottom">Search</button>
   </div>
   </form>
