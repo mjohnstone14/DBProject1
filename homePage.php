6d294a5535df45aecf8cbd4e309f2b318577c948
mjohnstone14 <mjohnstone@eaglerockschool.org>
2018-12-04 12:41:52 -0800

    sess

--- homePage.php
+++ homePage.php
@@ -266,16 +266,11 @@ function toggleFunction() {
 </script>
 
 <?php
-//remove PHPSESSID from browser
-if ( isset( $_COOKIE[session_name()] ) )
-setcookie( session_name(), “”, time()-3600, “/” );
 //clear session from globals
 $_SESSION = array();
 //clear session from disk
+session_unset();
 session_destroy();
-
-echo $_SESSION['username'];
-
 ?>
 
 </body>
