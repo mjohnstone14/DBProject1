99a4c8b6b2772136f95e54319c783740bc216393
unknown <harper.shapiro@gmail.com>
2018-12-05 13:24:20 -0800

    image display

--- userHome.php
+++ userHome.php
@@ -141,7 +141,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
       echo "<div class = 'w3-row-w3'>";
       echo "<div class = 'w3-col l3 s6'>";
       echo "<div class = 'w3-container'>";
-      echo "<form method='get' action='./imageDisplay?path=$path[imagePath]'>";
+      echo "<form method='get' action='./imageDisplay.php?path=$path[imagePath]'>";
       echo "<button type='submit'>";
       echo "<img src = $path[imagePath] height=100% width=100%</img>";
       echo "</button>";
